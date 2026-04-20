23f25fea517dc228eee3d8549922121f60c9f891
Coryf65 <Coryf65@users.noreply.github.com>
2026-04-19 19:18:36 -0500

    now checking for 2 char lexemes like >=, and !=

--- Book.CraftingInterpreters.lox/Scanner.cs
+++ Book.CraftingInterpreters.lox/Scanner.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Book.CraftingInterpreters.lox;
 
 public class Scanner
@@ -63,10 +65,43 @@ public class Scanner
             case ';':
                 AddToken(TokenType.SEMICOLON);
                 break;
+            case '!':
+                AddToken(Match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                break;
+            case '=':
+                AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                break;
+            case '<':
+                AddToken(Match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                break;
+            case '>':
+                AddToken(Match('=') ? TokenType.GREATER_EQUAL : TokenType.EQUAL);
+                break;
             default:
                 Locs.Error(_line, $"unexpected character '{c}'");
                 break;
         }
+    }
+
+    /// <summary>
+    /// determine if the current char is '>' or '>='
+    /// </summary>
+    /// <param name="expected"></param>
+    /// <returns></returns>
+    private bool Match(char expected)
+    {
+        bool isExpected = false;
+
+        if (IsAtEnd())
+            return isExpected;
+
+        if (_source[_current] != expected)
+            return isExpected;
+        
+        _current++;
+        isExpected = true;
+        
+        return isExpected;
     }
 
     /// <summary>
